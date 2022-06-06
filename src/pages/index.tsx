079e825150641a794c204c9c7e951fdcb3578eaf
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-06 20:11:41 -0300

    feat: Criado o header do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+   <>
+    <Head>
+      <title>Ig.news</title>
+    </Head>
+    <div>
+
+     </div>
+   </>
+  )
+}
+
+export default Home
